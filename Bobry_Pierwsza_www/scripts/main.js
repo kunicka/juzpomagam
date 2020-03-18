178bab92e098ba4c6b1d30f1546f7d87b679ac8f
Agnieszka <kuncika2000@gmail.com>
2020-03-18 18:34:39 +0100

    My first website

---
+++ Bobry_Pierwsza_www/scripts/main.js
@@ -0,0 +1,87 @@
+// var myHeading = document.querySelector('p');
+// myHeading.textContent = 'Heloł!';
+//comments in JS
+/*other comment*/
+
+/*
+var a = 5;
+console.log(a);
+a=10;
+console.log(a);
+
+var b = "Hej";
+console.log(b); 
+b = "Hello World";
+console.log(b); 
+
+var c = true;
+console.log(c);
+c = false;
+console.log(c);
+
+const PI = 3.14;
+console.log(PI);
+
+
+var num1 = 5;
+var num2 = 10;
+var num3 = 15;
+console.log(num1);
+console.log(num2);
+console.log(num3);
+num1 = num2 = num3 = 20;
+console.log(num1);
+console.log(num2);
+console.log(num3);
+
+var greeting = "Cześć, ";
+greeting += "Agnieszka";
+greeting += "!";
+console.log(greeting);
+
+var i = 0;
+i++;
+console.log(i);
+
+var n1 = 0;
+var n2 = "0";
+console.log(n1==n2);
+
+var n3 = 0;
+var n4 = "0";
+console.log(n3===n4);
+
+var n3 = 10;
+console.log(typeof n3);
+n3 = "abcd"
+console.log(typeof n3);
+
+var n4 = 4;
+var n5 = 5;
+var n6 = 6;
+console.log(n4 ==n5) && (n5==n6);
+*/
+
+
+var number1 = 0;
+var number2 = number1 + 4;
+var wasUserLogged = false;
+
+for(var i = 0; i < 5; i++) {
+    
+    console.log(number1);
+}
+
+for (var i = 0; i < 5; i++) {
+    
+    console.log("Wykonałam się " + i +" razy");
+}
+
+var element = window.document.getElementById("whatever");
+debugger;
+element.innerHTML = "Coś innego";
+
+
+
+
+
